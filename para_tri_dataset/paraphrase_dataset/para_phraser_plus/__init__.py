468d26adf5e0e757e3b507ffe46fc5f0868ff0f3
Андрей Козлюк <andrey.kozlyuk@sitesoft.ru>
2022-03-29 01:19:37 +0500

    ParaPhraserPlus датасет в виде sql базы данных

--- para_tri_dataset/paraphrase_dataset/para_phraser_plus/__init__.py
+++ para_tri_dataset/paraphrase_dataset/para_phraser_plus/__init__.py
@@ -1,3 +1,4 @@
 from para_tri_dataset.paraphrase_dataset.para_phraser_plus.file_dataset import ParaPhraserPlusFileDataset
+from para_tri_dataset.paraphrase_dataset.para_phraser_plus.sql_dataset import ParaPhraserPlusSQLDataset
 
-__all__ = ["ParaPhraserPlusFileDataset"]
+__all__ = ["ParaPhraserPlusFileDataset", "ParaPhraserPlusSQLDataset"]
